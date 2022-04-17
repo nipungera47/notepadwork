9d4fc4da719a267b138428b73b76fa4a9cae1889
nipun gera <nipungera7777@gmail.com>
2022-04-18 00:49:41 +0530

    laningpage

--- frontend/src/Screens/LandingPage/LandingPage.js
+++ frontend/src/Screens/LandingPage/LandingPage.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import "./LandingPage.css";
 
+import { Container,Row,Col,Button } from 'react-bootstrap';
+
 const LandingPage = () => {
   return (
     <div className='main'>
 
+   <Container>
+       <Row>
+           <div className="intro-text">
+               <div>
+                    <h1 className='title text-center'>Welcome to CoolNotePad</h1>
+                    <p className='sub-title text-center'> One place for all notes</p>
+               </div>
+
+                <div className="button-container">
+                    <a href="/login">
+                        <Button size="lg" className='landingButton'> Login </Button>
+                    </a>
+                    <a href="/register">
+                        <Button size="lg" className='landingButton' variant='outline-primary'> Register</Button>
+                    </a>
+                </div>
+
+
+
+           </div>
+       </Row>
+   </Container>
 
     </div>
   )
